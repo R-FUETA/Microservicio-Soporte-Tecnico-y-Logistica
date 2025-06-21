6ff53c782018805c8070581f6ca2ded9d4b77feb
R-FUETA <raul.a.fuenzalida@gmail.com>
2025-06-21 16:10:33 -0400

    1.1.2

---
+++ src/main/java/com/edutech_innovators_spa/SoporteTecnicoLogisticaApplication.java
@@ -0,0 +1,12 @@
+package com.edutech_innovators_spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteTecnicoLogisticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoporteTecnicoLogisticaApplication.class, args);
+    }
+}
